fcf70fa05d64b405230e087d753a3f8383eaffb7
JoanaNahorny <joananahorny@gmail.com>
2022-02-07 20:51:49 -0300

    Estrtutura de Dados - atualizações

--- estruturadados-algoritmos-java/src/com/loiane/estruturadados/fila/teste/Aula25.java
+++ estruturadados-algoritmos-java/src/com/loiane/estruturadados/fila/teste/Aula25.java
@@ -7,20 +7,20 @@ public class Aula25 {
 
 	public static void main(String[] args) {
 
-		Queue<Paciente> filaComPrioridade = new PriorityQueue<>(
-				//classe anonima  instanciada com metodos
-				new Comparator<Paciente>() {
-					@Override
-					public int compare(Paciente p1, Paciente p2){
-						return Integer.valueOf(p1.getPrioridade()).compareTo(p2);
-					}
-				}
-		);
-		
-		filaComPrioridade.add(new Paciente("A", 2));
-		filaComPrioridade.add(new Paciente("B", 1));
-
-		System.out.println(filaComPrioridade);
+//		Queue<Paciente> filaComPrioridade = new PriorityQueue<>(
+//				//classe anonima  instanciada com metodos
+//				/new Comparator<Paciente>() {
+//					@Override
+//					public int compare(Paciente p1, Paciente p2){
+//						return Integer.valueOf(p1.getPrioridade()).compareTo(p2);
+//					}
+//				}
+//		);
+//		
+//		filaComPrioridade.add(new Paciente("A", 2));
+//		filaComPrioridade.add(new Paciente("B", 1));
+//
+//		System.out.println(filaComPrioridade);
 		
 	}
 
